e9efa4ff7ad6076e4383fe422ef2ab3ac15fd663
shivam-futureapp <shivam@valleysc.in>
2023-12-16 00:51:17 +0545

    new changes

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <h1>Welcome to Valley Startup Consultancy</h1>
-      <p>We Are Tech Enthusiast</p>
+    <div className={`${styles.container} ${styles.fadeInAnimation}`}>
+      <h1 className={styles.title}>Welcome to Valley Startup Consultancy</h1>
+      <p>We Are Tech Enthusiast and We Innovate The World</p>
     </div>
   );
 };
