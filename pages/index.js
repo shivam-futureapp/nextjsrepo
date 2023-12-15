65d3486259390208e86ce3b398ef488202f6b189
shivam-futureapp <shivam@valleysc.in>
2023-12-15 19:42:22 +0545

    added code to repo

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to Valley Startup Consultancy</h1>
+      <p>This is a sample Next.js application.</p>
+    </div>
+  );
+};
+
+export default Home;
